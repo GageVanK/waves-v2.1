db7148859f529b19293c7f1a3bf70a9b507925de
gage <87737875+GageVanK@users.noreply.github.com>
2023-05-15 02:15:48 +0000

    Update contexts.ts

--- contexts.ts
+++ contexts.ts
@@ -3,9 +3,11 @@ import { createContext } from 'react';
 interface UserContextType {
   currentUser: any | null;
   alternateUsers: any | null;
+  isLoading: boolean;
 }
 
 export const UserContext = createContext<UserContextType>({
   currentUser: null,
   alternateUsers: null,
+  isLoading: false,
 });
